fc66f34378d91fc797c3e0819c1156026feda768
Galiverking <119554919+Galiverking@users.noreply.github.com>
2025-03-15 23:04:40 +0700

    Add files via upload

--- js/album.js
+++ js/album.js
@@ -3,7 +3,7 @@ document.addEventListener('DOMContentLoaded', function() {
     const albumId = urlParams.get('album');
     const albumTitle = urlParams.get('title');
     const albumImages = {
-        album1: ['images/memory1.png', 'images/memory2.png', 'images/memory3.png'],
+        album1: ['images/album1/1.jpg', 'images/album1/2.jpg', 'images/album1/3.jpg', 'images/album1/4.jpg', 'images/album1/5.jpg', 'images/album1/6.jpg', 'images/album1/7.jpg', 'images/album1/8.jpg', 'images/album1/9.jpg', 'images/album1/10.jpg', 'images/album1/11.jpg', 'images/album1/12.jpg', 'images/album1/13.jpg', 'images/album1/14.jpg', 'images/album1/15.jpg'],
         album2: ['images/memory4.png', 'images/memory5.png', 'images/memory6.png'],
         album3: ['images/memory7.png', 'images/memory8.png', 'images/memory9.png']
     };
